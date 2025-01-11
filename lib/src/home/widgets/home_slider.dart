5cf9988ea9abe39000b8f433cebfcdcc231f1228
Fikri Zaki Aditama, S.Si. <fikriyomann@gmail.com>
2025-01-11 20:25:14 +0700

    up

--- lib/src/home/widgets/home_slider.dart
+++ lib/src/home/widgets/home_slider.dart
@@ -1,10 +1,36 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:fashion_app/common/utils/kcolors.dart';
+import 'package:fashion_app/const/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeSlider extends StatelessWidget {
   const HomeSlider({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ClipRRect(
+      borderRadius: kRadiusAll,
+      child: Stack(
+        children: [
+          SizedBox(
+            height: ScreenUtil().screenHeight * 0.16,
+            width: ScreenUtil().screenWidth,
+            child: ImageSlideshow(
+              indicatorColor: Kolors.kPrimaryLight,
+              onPageChanged: (p) {},
+              children: List.generate(images.length, (i) {
+                return CachedNetworkImage(
+                  placeholder: placeholder,
+                  errorWidget: errorWidget,
+                  imageUrl: images[i],
+                );
+              }),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
